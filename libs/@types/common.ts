66281965309773276e40ba97bdca3f4a405a3490
Bagas Ranggen <bagas.ranggen@gmail.com>
2026-02-13 14:33:02 +0700

    refactor: add helper libs

---
+++ libs/@types/common.ts
@@ -0,0 +1,44 @@
+import React, { JSX } from 'react';
+import { Metadata } from 'next';
+
+export type ObjectProps<Props> = { [key: string]: Props };
+
+export type TypesProps<Type, Props> = {
+    type: Type;
+} & Props;
+
+export type PageProps = {
+    params: Promise<ObjectProps<string | string[]>>;
+    searchParams?: Promise<ObjectProps<string | string[] | undefined>>;
+};
+
+export type PageDataParamsProps = {
+    type?: string;
+    uri?: string;
+    slug?: string;
+};
+
+export type PageUriItemProps = {
+    slug: string[];
+};
+
+export type PageDataProps<Props> = {
+    // page: {
+    //     seomatic: any;
+    // };
+    meta?: Metadata;
+} & (Pick<PageDataParamsProps, 'type'> & Props);
+
+export type Component<Props> = { (props: Props): React.ReactElement | null };
+
+export type RefComponent<Props, Element> = React.ForwardRefExoticComponent<Props & React.RefAttributes<Element>>;
+
+export type ClassnameProps = { className?: string };
+
+export type ArrayStringProps = string[] | string;
+
+export type ElementTagsProps = keyof JSX.IntrinsicElements;
+
+export type ResponsiveProps = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl';
+
+export type BreakpointsProps = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl' | 'xxxl';
