a9da3ebbcaa0d51be42c7f4e33cb3b648279bf80
Bagas Ranggen <bagas.ranggen@gmail.com>
2026-02-13 20:48:31 +0700

    refactor: add heading component

---
+++ components/common/Heading/Base/index.tsx
@@ -0,0 +1,34 @@
+import React, { forwardRef, PropsWithChildren } from 'react';
+
+import { ArrayStringProps, ElementTagsProps } from '@/libs/@types';
+import { joinArrayString } from '@/libs/utils';
+
+export type BaseProps = {
+    as?: Extract<ElementTagsProps, 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'>;
+    variant?: 'title';
+} & (React.HTMLAttributes<HTMLHeadingElement> & PropsWithChildren);
+
+const Base = forwardRef<HTMLHeadingElement, BaseProps>(
+    ({ as: Heading = 'h2', className, variant, children, ...props }, ref) => {
+        let headingClass: ArrayStringProps = [];
+        // if (variant) headingClass.push('heading');
+        // if (variant === 'title') headingClass.push('heading--title');
+        if (className) headingClass.push(className);
+        headingClass = joinArrayString(headingClass);
+
+        let headingProps = props;
+        if (headingClass) headingProps = Object.assign(headingProps, { className: headingClass });
+
+        return (
+            <Heading
+                ref={ref}
+                {...headingProps}>
+                {children}
+            </Heading>
+        );
+    }
+);
+
+Base.displayName = 'Base';
+
+export default Base;
