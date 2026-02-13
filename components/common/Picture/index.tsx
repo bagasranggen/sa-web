a3dd160486002281378991adce63b11624985c96
Bagas Ranggen <bagas.ranggen@gmail.com>
2026-02-14 01:13:37 +0700

    refactor: update picture component

--- components/common/Picture/index.tsx
+++ components/common/Picture/index.tsx
@@ -1,7 +1,7 @@
 import React, { forwardRef } from 'react';
 import { ImageProps } from 'next/image';
 
-import { ArrayStringProps } from '@/libs/@types';
+import { ArrayStringProps, ClassnameProps } from '@/libs/@types';
 import { joinArrayString } from '@/libs/utils';
 
 import PictureSource from '@/components/common/Picture/PictureSource';
@@ -14,43 +14,53 @@ export type BaseItemProps = {
 } & ImageProps;
 
 export type BaseProps = {
-    className?: string;
+    className?: ClassnameProps['className'];
+    imageClassName?: ClassnameProps['className'];
     style?: React.CSSProperties;
     events?: React.DOMAttributes<HTMLPictureElement>;
     items: BaseItemProps[];
 };
 
-const Base = forwardRef<HTMLPictureElement, BaseProps>(({ className, items, style, events, ...props }, ref) => {
-    let pictureClass: ArrayStringProps = [];
-    if (className) pictureClass.push(className);
-    pictureClass = joinArrayString(pictureClass);
+const Base = forwardRef<HTMLPictureElement, BaseProps>(
+    ({ className, imageClassName, items, style, events, ...props }, ref) => {
+        let pictureClass: ArrayStringProps = [];
+        if (className) pictureClass.push(className);
+        pictureClass = joinArrayString(pictureClass);
 
-    let pictureProps = props;
-    if (style) pictureProps = { ...pictureProps, style: style };
-    if (events) pictureProps = { ...pictureProps, ...events };
+        let pictureProps = props;
+        if (style) pictureProps = { ...pictureProps, style: style };
+        if (events) pictureProps = { ...pictureProps, ...events };
 
-    return (
-        <picture
-            ref={ref}
-            {...(pictureClass ? { className: pictureClass } : {})}
-            {...pictureProps}>
-            {items.map((item, i) => {
-                const Image = items.length - 1 === i ? PictureImage : PictureSource;
-                const { alt, title, ...restItem } = item as any;
+        return (
+            <picture
+                ref={ref}
+                {...(pictureClass ? { className: pictureClass } : {})}
+                {...pictureProps}>
+                {items.map((item, i) => {
+                    const isLast = items.length - 1 === i;
 
-                let props = { ...restItem, alt };
-                if (!alt && title) props = { ...props, alt: title };
+                    const Image = items.length - 1 === i ? PictureImage : PictureSource;
+                    const { alt, title, className, ...restItem } = item as any;
 
-                return (
-                    <Image
-                        key={i}
-                        {...props}
-                    />
-                );
-            })}
-        </picture>
-    );
-});
+                    let imgClass: ArrayStringProps = className ? [className] : [];
+                    if (imageClassName) imgClass.push(imageClassName);
+                    imgClass = joinArrayString(imgClass);
+
+                    let props: any = { ...restItem, alt };
+                    if (!alt && title) props = Object.assign(props, { alt: title });
+                    if (imgClass) props = Object.assign(props, { className: imgClass });
+
+                    return (
+                        <Image
+                            key={i}
+                            {...props}
+                        />
+                    );
+                })}
+            </picture>
+        );
+    }
+);
 
 Base.displayName = 'Base';
 export default Base;
