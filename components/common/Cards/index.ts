53ceef72d4f8a1656f928f958fde701f05a9c4dc
Bagas Ranggen <bagas.ranggen@gmail.com>
2026-02-14 17:39:19 +0700

    refactor: add cards media component

--- components/common/Cards/index.ts
+++ components/common/Cards/index.ts
@@ -1,11 +1,14 @@
 import { Component } from '@/libs/@types';
 
 import Highlight, { HighlightProps } from '@/components/common/Cards/Highlight';
+import Media, { MediaProps } from '@/components/common/Cards/Media';
 
 export type * from '@/components/common/Cards/Highlight';
+export type * from '@/components/common/Cards/Media';
 
 type CardsComposition = {
     Highlight: Component<HighlightProps>;
+    Media: Component<MediaProps>;
 };
 
-export default Object.assign<{}, CardsComposition>({}, { Highlight });
+export default Object.assign<{}, CardsComposition>({}, { Highlight, Media });
