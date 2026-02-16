2fab5ce15e61ea9722d3c4c17dc941d27484a3ce
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2026-02-16 17:25:43 +0700

    refactor: create base logic for products' filtering

--- components/pages/ProductListingIndex/index.tsx
+++ components/pages/ProductListingIndex/index.tsx
@@ -5,12 +5,15 @@ import { usePathname, useRouter } from 'next/navigation';
 
 import { PRODUCT_LISTING_LOAD } from '@/libs/mock';
 import { IntersectionEvents, ParamsEvents } from '@/libs/hooks';
+import { convertObjectToSearchParamsQuery } from '@/libs/utils';
 
 import Container from '@/components/common/Container';
 import Heading from '@/components/common/Heading';
 import Cards, { ThumbnailProps } from '@/components/common/Cards';
-import Button from '@/components/common/Button';
 import Loader from '@/components/common/Loader';
+import ProductListingFilter, {
+    ProductListingFilterProps,
+} from '@/components/pages/ProductListingIndex/ProductListingFilter';
 
 export type ProductListingIndexProps = {
     entries: {
@@ -19,6 +22,8 @@ export type ProductListingIndexProps = {
 };
 
 const ProductListingIndex = ({ entries }: ProductListingIndexProps): React.ReactElement => {
+    const [filter, setFilter] = useState<ProductListingFilterProps['activeFilter']>({});
+
     const router = useRouter();
     const pathname = usePathname();
 
@@ -26,6 +31,21 @@ const ProductListingIndex = ({ entries }: ProductListingIndexProps): React.React
     const [isLoading, setIsLoading] = useState(false);
     const [page, setPage] = useState(0);
     const [listingLoadItems, setListingLoadItems] = useState<ThumbnailProps['items']>([]);
+
+    const resetFilterIsActive = useMemo(() => {
+        let data = false;
+
+        const filterArr = Object.entries(filter ?? {});
+        if (filter && filterArr.length > 0) {
+            filterArr.forEach(([key, value]) => {
+                if (key === 'sort') return;
+
+                if (value && Array.isArray(value) && value.length > 0) data = true;
+            });
+        }
+
+        return data;
+    }, [filter]);
 
     const listingItems = useMemo(() => {
         const data = [];
@@ -41,8 +61,10 @@ const ProductListingIndex = ({ entries }: ProductListingIndexProps): React.React
     const pageLoadHandler = ({ page, scroll = false }: { page: number; scroll?: boolean }) => {
         setIsLoading(true);
 
+        const searchQuery = convertObjectToSearchParamsQuery({ obj: { page: page } });
+
         let path = pathname;
-        if (page > 0) path += `?page=${page}`;
+        if (page > 0 && searchQuery) path += searchQuery;
 
         router.push(path, { scroll });
     };
@@ -52,16 +74,22 @@ const ProductListingIndex = ({ entries }: ProductListingIndexProps): React.React
             <Suspense fallback={null}>
                 <IntersectionEvents
                     ref={loadMoreRef}
-                    onIntersection={() => pageLoadHandler({ page: page + 1 })}
+                    onIntersection={() => {
+                        if (!isLoading) {
+                            pageLoadHandler({ page: page + 1, scroll: false });
+                        }
+                    }}
                 />
 
                 {/* TODO: change items fetching with real data later on */}
                 <ParamsEvents
                     onChange={({ params }) => {
-                        const page = params?.page ? Number(params.page) : undefined;
+                        const { page, ...rest } = params;
 
-                        if (page && page > 0) {
-                            setPage(page);
+                        const paramsPage = page ? Number(page) : undefined;
+
+                        if (paramsPage && paramsPage > 0) {
+                            setPage(paramsPage);
 
                             setTimeout(() => {
                                 setIsLoading(false);
@@ -69,13 +97,27 @@ const ProductListingIndex = ({ entries }: ProductListingIndexProps): React.React
                             }, 1000);
                         }
 
-                        if (!page) {
+                        if (!paramsPage) {
                             setPage(0);
 
                             setTimeout(() => {
                                 setIsLoading(false);
                                 setListingLoadItems([]);
                             }, 1000);
+                        }
+
+                        const restArr = Object.entries(rest);
+
+                        if (restArr.length > 0) {
+                            let tmp = {};
+
+                            restArr.forEach(([key, value]) => {
+                                tmp = Object.assign(tmp, {
+                                    [key]: value.split(','),
+                                });
+                            });
+
+                            setFilter((prevState) => ({ ...prevState, ...tmp }));
                         }
                     }}
                 />
@@ -90,23 +132,54 @@ const ProductListingIndex = ({ entries }: ProductListingIndexProps): React.React
             </Container>
 
             <Container className="mt-4 mb-15">
-                {/* TODO: add filter here */}
-                {/*<div className="mb-2">*/}
-                {/*    <Button.Block*/}
-                {/*        as="button"*/}
-                {/*        color="dark"*/}
-                {/*        onClick={() => {*/}
-                {/*            pageLoadHandler({ page: 0 });*/}
-                {/*        }}>*/}
-                {/*        reset*/}
-                {/*    </Button.Block>*/}
-                {/*</div>*/}
+                <ProductListingFilter
+                    className="mb-2"
+                    activeFilter={filter}
+                    filters={[
+                        {
+                            handle: 'color',
+                            children: 'Color',
+                            checkbox: [
+                                { label: 'Red', value: '#555' },
+                                { label: 'Blue', value: '#666' },
+                            ],
+                        },
+                        {
+                            handle: 'size',
+                            children: 'Size',
+                            checkbox: [
+                                { label: 'Extra Small', value: 'xs' },
+                                { label: 'Small', value: 'sm' },
+                            ],
+                        },
+                    ]}
+                    onOpenChange={(open, form) => {
+                        if (!open) {
+                            const searchQuery = convertObjectToSearchParamsQuery({
+                                obj: form,
+                                removeParams: ['page'],
+                            });
+
+                            let path = pathname;
+                            if (searchQuery) path += searchQuery;
+
+                            router.push(path);
+                        }
+                    }}
+                    reset={{
+                        active: resetFilterIsActive,
+                        onResetFilters: () => {
+                            router.push(pathname);
+                            setFilter({});
+                        },
+                    }}
+                />
 
                 {listingItems && listingItems.length > 0 && (
                     <>
                         <Cards.Thumbnail items={listingItems} />
 
-                        {isLoading && <Loader className="flex flex-col items-center my-3">Loading</Loader>}
+                        {isLoading && <Loader className="flex flex-col items-center mt-12 mb-3">Loading</Loader>}
 
                         <div ref={loadMoreRef} />
                     </>
