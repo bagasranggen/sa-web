9b49d24e38935562c06d8c5d6a887a5476daf6b0
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2026-02-19 13:05:07 +0700

    refactor: update helper libs

--- libs/factory/createPicsumImage.ts
+++ libs/factory/createPicsumImage.ts
@@ -1,13 +1,14 @@
-export const createPicsumImage = ({
-    id = 239,
-    width = 600,
-    height = 450,
-    alt,
-}: { alt?: string } & Partial<Record<'id' | 'width' | 'height', number>>) => {
+export type CreatePicsumImageProps = {
+    alt?: string;
+    media?: number;
+} & Partial<Record<'id' | 'width' | 'height', number>>;
+
+export const createPicsumImage = ({ id = 239, width = 600, height = 450, alt, media }: CreatePicsumImageProps) => {
     return {
         src: `https://picsum.photos/id/${id}/${width}/${height}`,
         width,
         height,
         alt: alt ?? `image ${id}`,
+        ...(media ? { media } : {}),
     };
 };
