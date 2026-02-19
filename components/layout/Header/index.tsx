4f1d7b74817fbcd31b21e6075b5fd22f87ecf411
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2026-02-19 13:07:13 +0700

    refactor: update header logo

--- components/layout/Header/index.tsx
+++ components/layout/Header/index.tsx
@@ -9,7 +9,11 @@ const Header = ({}: HeaderProps): React.ReactElement => {
     return (
         <nav className="nav">
             <Container className="flex justify-between">
-                <div>LOGO</div>
+                <Button
+                    as="anchor"
+                    href="/">
+                    LOGO
+                </Button>
 
                 <div className="nav__links">
                     <Button
