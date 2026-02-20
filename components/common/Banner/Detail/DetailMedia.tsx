d6c67232128601bb401e77dfae4e326b23aa6fb4
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2026-02-20 15:50:07 +0700

    refactor: add carousel arrow

--- components/common/Banner/Detail/DetailMedia.tsx
+++ components/common/Banner/Detail/DetailMedia.tsx
@@ -1,22 +1,22 @@
 'use client';
 
-import React, { Ref, useState } from 'react';
+import React, { Ref, useRef, useState } from 'react';
+
+import { createPicsumImage } from '@/libs/factory';
 
 import { useMeasure } from 'react-use';
 import { Swiper } from 'swiper/types';
-import { Pagination, Thumbs } from 'swiper/modules';
-
-import 'swiper/css/pagination';
-
+import { Pagination, Thumbs, Navigation } from 'swiper/modules';
 import Lightbox from 'yet-another-react-lightbox';
 
+import 'swiper/css/pagination';
 import 'yet-another-react-lightbox/styles.css';
 
 import Columns from '@/components/common/Columns';
 import Picture, { BaseProps } from '@/components/common/Picture';
 import Carousel from '@/components/common/Carousel';
 import Button from '@/components/common/Button';
-import { createPicsumImage } from '@/libs/factory';
+import Icon from '@/components/common/Icon';
 
 export type DetailMediaItemProps = BaseProps['items'];
 
@@ -24,8 +24,13 @@ export type DetailMediaProps = {} & Partial<Record<'thumbnail' | 'media', Detail
 
 const DetailMedia = ({ thumbnail, media }: DetailMediaProps): React.ReactElement | null => {
     const [thumbs, setThumbs] = useState<null | Swiper>(null);
+    const [preview, setPreview] = useState<null | Swiper>(null);
     const [openLightbox, setOpenLightbox] = useState<boolean>(false);
     const [lightboxIndex, setLightboxIndex] = useState<number>(-1);
+    const thumbPrevRef = useRef(null);
+    const thumbNextRef = useRef(null);
+    const previewPrevRef = useRef(null);
+    const previewNextRef = useRef(null);
     const [previewRef, { height }] = useMeasure();
 
     let thumbStyle: React.CSSProperties = {};
@@ -56,14 +61,21 @@ const DetailMedia = ({ thumbnail, media }: DetailMediaProps): React.ReactElement
                             spaceBetween={10}
                             autoHeight
                             pagination={{ clickable: true }}
-                            modules={[Pagination, Thumbs]}
+                            modules={[Pagination, Thumbs, Navigation]}
+                            navigation={{
+                                prevEl: thumbPrevRef.current,
+                                nextEl: thumbNextRef.current,
+                                disabledClass: 'opacity-40 pointer-events-none',
+                            }}
                             breakpoints={{
                                 0: {
                                     direction: 'horizontal',
+                                    navigation: { enabled: false },
                                     pagination: { enabled: true },
                                 },
                                 768: {
                                     direction: 'vertical',
+                                    navigation: { enabled: true },
                                     pagination: { enabled: false },
                                 },
                             }}
@@ -71,8 +83,26 @@ const DetailMedia = ({ thumbnail, media }: DetailMediaProps): React.ReactElement
                                 return {
                                     children: <Picture items={item} />,
                                 };
-                            })}
-                        />
+                            })}>
+                            {thumbnail.length > 3 && (
+                                <div className="max-md:hidden">
+                                    <Button
+                                        ref={thumbPrevRef}
+                                        className="swiper__navigation swiper__navigation--prev">
+                                        <Icon.Chevron
+                                            direction="up"
+                                            withCircle={{ color: 'primary', size: 'sm' }}
+                                        />
+                                    </Button>
+
+                                    <Button
+                                        ref={thumbNextRef}
+                                        className="swiper__navigation swiper__navigation--next">
+                                        <Icon.Chevron withCircle={{ color: 'primary', size: 'sm' }} />
+                                    </Button>
+                                </div>
+                            )}
+                        </Carousel>
                     </div>
                 </Columns.Column>
 
@@ -81,69 +111,95 @@ const DetailMedia = ({ thumbnail, media }: DetailMediaProps): React.ReactElement
                     className="order-1 md:order-2">
                     <div ref={previewRef as Ref<HTMLDivElement>}>
                         <Carousel
+                            className="banner__preview"
+                            onSwiper={setPreview}
                             thumbs={{ swiper: thumbs }}
-                            modules={[Thumbs]}
+                            modules={[Thumbs, Navigation]}
+                            navigation={{
+                                enabled: true,
+                                prevEl: previewPrevRef.current,
+                                nextEl: previewNextRef.current,
+                                disabledClass: 'opacity-40 pointer-events-none',
+                            }}
+                            breakpoints={{
+                                0: {
+                                    allowTouchMove: true,
+                                },
+                                768: {
+                                    allowTouchMove: false,
+                                },
+                            }}
                             items={media.map((item: DetailMediaItemProps, i) => {
                                 return {
                                     // children: <Picture items={item} />,
                                     children: (
                                         <Picture
                                             items={item}
-                                            onClick={() => {
-                                                setOpenLightbox(true);
-                                                setLightboxIndex(i);
-                                            }}
-                                            className="cursor-pointer"
+                                            // onClick={() => {
+                                            // setOpenLightbox(true);
+                                            // setLightboxIndex(i);
+                                            // }}
+                                            // className="cursor-pointer"
                                         />
                                     ),
-                                    // children: (
-                                    //     <Button
-                                    //         as="button"
-                                    //         className="block" onClick={}>
-                                    //         <Picture items={item} />
-                                    //     </Button>
-                                    // ),
                                 };
-                            })}
-                        />
+                            })}>
+                            {media.length > 1 && (
+                                <div className="swiper__navigation">
+                                    <Button ref={previewPrevRef}>
+                                        <Icon.Arrow
+                                            direction="left"
+                                            withCircle={{ color: 'primary' }}
+                                        />
+                                    </Button>
+
+                                    <Button ref={previewNextRef}>
+                                        <Icon.Arrow withCircle={{ color: 'primary', size: 'lg' }} />
+                                    </Button>
+                                </div>
+                            )}
+                        </Carousel>
                     </div>
                 </Columns.Column>
             </Columns>
 
-            <Button.Block
-                as="button"
-                onClick={() => {
-                    setOpenLightbox(true);
-                }}>
-                OPEN
-            </Button.Block>
+            {/*<Button.Block*/}
+            {/*    as="button"*/}
+            {/*    onClick={() => {*/}
+            {/*        setOpenLightbox(true);*/}
+            {/*    }}>*/}
+            {/*    OPEN*/}
+            {/*</Button.Block>*/}
 
-            <Lightbox
-                open={openLightbox}
-                index={lightboxIndex}
-                close={() => {
-                    setOpenLightbox(false);
-                    setLightboxIndex(-1);
-                }}
-                // slides={[createPicsumImage({}), createPicsumImage({ id: 33, width: 1000, height: 1400 })]}
-                // slides={media.map((item) => item[0])}
-                slides={media}
-                // slides={[
-                //     {
-                //         // type: 'media',
-                //         test: [createPicsumImage({})],
-                //     },
-                // ]}
-                render={{
-                    slide: (props) => {
-                        console.log({ props });
-
-                        // return <>CUSTOM</>;
-                        // return <Picture items={[props.slide]} />;
-                        return <Picture items={props.slide} />;
-                    },
-                }}
-            />
+            {/*<Lightbox*/}
+            {/*    open={openLightbox}*/}
+            {/*    index={lightboxIndex}*/}
+            {/*    close={() => {*/}
+            {/*        setOpenLightbox(false);*/}
+            {/*        setLightboxIndex(-1);*/}
+            {/*    }}*/}
+            {/*    slides={[*/}
+            {/*        createPicsumImage({ width: 1000, height: 1400 }),*/}
+            {/*        createPicsumImage({ id: 33, width: 1000, height: 1400 }),*/}
+            {/*    ]}*/}
+            {/*    // slides={media.map((item) => item[0])}*/}
+            {/*    // slides={media}*/}
+            {/*    // slides={[*/}
+            {/*    //     {*/}
+            {/*    //         // type: 'media',*/}
+            {/*    //         test: [createPicsumImage({})],*/}
+            {/*    //     },*/}
+            {/*    // ]}*/}
+            {/*    // render={{*/}
+            {/*    //     slide: (props) => {*/}
+            {/*    //         console.log({ props });*/}
+            {/*    //*/}
+            {/*    //         // return <>CUSTOM</>;*/}
+            {/*    //         // return <Picture items={[props.slide]} />;*/}
+            {/*    //         return <Picture items={props.slide} />;*/}
+            {/*    //     },*/}
+            {/*    // }}*/}
+            {/*/>*/}
         </>
     );
 };
