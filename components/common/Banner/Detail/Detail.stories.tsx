3056258fd809e09bce0dfdfa75e3d9ea6727106d
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2026-02-20 15:49:19 +0700

    refactor: banner detail stories

---
+++ components/common/Banner/Detail/Detail.stories.tsx
@@ -0,0 +1,50 @@
+import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+
+// import { fn } from 'storybook/test';
+
+import { CAROUSEL_MEDIA_PREVIEW, CAROUSEL_MEDIA_THUMB } from '@/libs/mock';
+
+import Detail from './index';
+import Container from '@/components/common/Container';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+    title: 'Banner/Detail',
+    component: Detail,
+    decorators: [
+        (Story) => (
+            <div style={{ maxWidth: '100vw' }}>
+                <Container className="my-5">
+                    <Story />
+                </Container>
+            </div>
+        ),
+    ],
+    parameters: {
+        // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+        layout: 'fullscreen',
+    },
+    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+    // tags: ['autodocs'],
+    // More on argTypes: https://storybook.js.org/docs/api/argtypes
+    argTypes: {
+        // backgroundColor: { control: 'color' },
+    },
+    // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#story-args
+    // args: { onClick: fn() },
+} satisfies Meta<typeof Detail>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Default: Story = {
+    args: {
+        carousel: {
+            thumbnail: CAROUSEL_MEDIA_THUMB,
+            media: CAROUSEL_MEDIA_PREVIEW,
+        },
+        price: 'Rp300.000/3day(s)',
+        children: 'Heading',
+    },
+};
