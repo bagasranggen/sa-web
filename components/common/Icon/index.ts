57b0d14bcbba63a076fd6f4e76b578613f6bca5c
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2026-02-20 15:47:05 +0700

    refactor: add icon chevron

--- components/common/Icon/index.ts
+++ components/common/Icon/index.ts
@@ -2,13 +2,16 @@ import { Component } from '@/libs/@types';
 
 import Arrow, { ArrowProps } from '@/components/common/Icon/Arrow';
 import Check, { CheckProps } from '@/components/common/Icon/Check';
+import Chevron, { ChevronProps } from '@/components/common/Icon/Chevron';
 
 export type * from '@/components/common/Icon/Arrow';
 export type * from '@/components/common/Icon/Check';
+export type * from '@/components/common/Icon/Chevron';
 
 type IconComposition = {
     Arrow: Component<ArrowProps>;
     Check: Component<CheckProps>;
+    Chevron: Component<ChevronProps>;
 };
 
-export default Object.assign<{}, IconComposition>({}, { Arrow, Check });
+export default Object.assign<{}, IconComposition>({}, { Arrow, Check, Chevron });
