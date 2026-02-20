fbaba3ab75f7295a09efcc4e17e1b96eae5e43fa
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2026-02-20 15:45:54 +0700

    refactor: add icon arrow

--- components/common/Icon/index.ts
+++ components/common/Icon/index.ts
@@ -1,11 +1,14 @@
 import { Component } from '@/libs/@types';
 
+import Arrow, { ArrowProps } from '@/components/common/Icon/Arrow';
 import Check, { CheckProps } from '@/components/common/Icon/Check';
 
+export type * from '@/components/common/Icon/Arrow';
 export type * from '@/components/common/Icon/Check';
 
 type IconComposition = {
+    Arrow: Component<ArrowProps>;
     Check: Component<CheckProps>;
 };
 
-export default Object.assign<{}, IconComposition>({}, { Check });
+export default Object.assign<{}, IconComposition>({}, { Arrow, Check });
