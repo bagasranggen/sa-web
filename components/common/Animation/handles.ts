214ac662fa6fed55c77aebfef42c34aff6cce15b
Bagas Ranggen <bagas.ranggen@gmail.com>
2026-02-21 15:20:48 +0700

    refactor: add fade animation

--- components/common/Animation/handles.ts
+++ components/common/Animation/handles.ts
@@ -1,12 +1,24 @@
 import { createAnimationHandles } from '@/libs/factory';
 
+import { fade, fadeIn } from '@/components/common/Animation/elements/fade';
 import { marquee } from '@/components/common/Animation/elements/marquee';
 
 export const ANIMATION_VARIANTS = {
+    FADE: 'fade',
+    FADE_IN: 'fade-in',
     MARQUEE: 'marquee',
 } as const;
 
 export const ANIMATION_HANDLES = {
+    ...createAnimationHandles({
+        handles: ANIMATION_VARIANTS.FADE,
+        animation: fade,
+        extendTimeline: true,
+    }),
+    ...createAnimationHandles({
+        handles: ANIMATION_VARIANTS.FADE_IN,
+        animation: fadeIn,
+    }),
     ...createAnimationHandles({
         handles: ANIMATION_VARIANTS.MARQUEE,
         animation: marquee,
--- libs/@types/animation.ts
+++ libs/@types/animation.ts
@@ -1,14 +1,17 @@
 import { ANIMATION_VARIANTS } from '@/components/common/Animation/handles';
+import { FadeConfigProps } from '@/components/common/Animation/elements/fade';
 import { MarqueeConfigProps } from '@/components/common/Animation/elements/marquee';
 
-export type BaseAnimationConfigProps<Type, Config> = {
+export type BaseAnimationCustomConfigProps<Type, Config> = {
     type?: Type;
     config?: Config;
 };
 
+export type BaseAnimationConfigProps = gsap.TweenVars;
+
 export type BaseAnimationProps = {
     element: HTMLElement[] | Element[] | any;
-    config?: gsap.TweenVars;
+    config?: BaseAnimationConfigProps;
     id?: number | string;
 };
 
@@ -16,7 +19,9 @@ export type AnimationProps =
     | ({
           type?: Exclude<
               (typeof ANIMATION_VARIANTS)[keyof typeof ANIMATION_VARIANTS],
-              typeof ANIMATION_VARIANTS.MARQUEE
+              typeof ANIMATION_VARIANTS.FADE | typeof ANIMATION_VARIANTS.FADE_IN | typeof ANIMATION_VARIANTS.MARQUEE
           >;
       } & Pick<BaseAnimationProps, 'config'>)
-    | BaseAnimationConfigProps<typeof ANIMATION_VARIANTS.MARQUEE, MarqueeConfigProps>;
+    | BaseAnimationCustomConfigProps<typeof ANIMATION_VARIANTS.FADE, FadeConfigProps>
+    | BaseAnimationCustomConfigProps<typeof ANIMATION_VARIANTS.FADE_IN, FadeConfigProps>
+    | BaseAnimationCustomConfigProps<typeof ANIMATION_VARIANTS.MARQUEE, MarqueeConfigProps>;
