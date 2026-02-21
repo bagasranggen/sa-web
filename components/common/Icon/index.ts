8c401699518079ebc566575820b708bc25f1247a
Bagas Ranggen <bagas.ranggen@gmail.com>
2026-02-21 21:09:55 +0700

    refactor: add hamburger icon

--- components/common/Icon/index.ts
+++ components/common/Icon/index.ts
@@ -3,15 +3,18 @@ import { Component } from '@/libs/@types';
 import Arrow, { ArrowProps } from '@/components/common/Icon/Arrow';
 import Check, { CheckProps } from '@/components/common/Icon/Check';
 import Chevron, { ChevronProps } from '@/components/common/Icon/Chevron';
+import Hamburger, { HamburgerProps } from '@/components/common/Icon/Hamburger';
 
 export type * from '@/components/common/Icon/Arrow';
 export type * from '@/components/common/Icon/Check';
 export type * from '@/components/common/Icon/Chevron';
+export type * from '@/components/common/Icon/Hamburger';
 
 type IconComposition = {
     Arrow: Component<ArrowProps>;
     Check: Component<CheckProps>;
     Chevron: Component<ChevronProps>;
+    Hamburger: Component<HamburgerProps>;
 };
 
-export default Object.assign<{}, IconComposition>({}, { Arrow, Check, Chevron });
+export default Object.assign<{}, IconComposition>({}, { Arrow, Check, Chevron, Hamburger });
