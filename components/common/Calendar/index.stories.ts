1e7fca1102901b5cae0d019f2874e31e42501661
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2026-02-24 22:50:01 +0700

    fix: build error

--- components/common/Calendar/index.stories.ts
+++ components/common/Calendar/index.stories.ts
@@ -17,7 +17,6 @@ const meta = {
     // More on argTypes: https://storybook.js.org/docs/api/argtypes
     argTypes: {},
     // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#story-args
-    args: { onClick: fn() },
 } satisfies Meta<typeof Calendar>;
 
 export default meta;
