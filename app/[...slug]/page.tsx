1c1d2115b9d45eede073455a66136b5c4e130f42
Bagas Ranggen <bagas.ranggen@gmail.com>
2026-03-01 15:18:23 +0700

    refactor: add dynamic pages

---
+++ app/[...slug]/page.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { PageProps } from '@/libs/@types';
+import { getPagesData, getUriFromParams } from '@/libs/utils';
+
+import DynamicElement from '@/components/common/DynamicElement';
+import { PAGES_INDEX_HANDLES } from '@/components/pages/handlesIndex';
+
+const Page = async ({ params: paramsProps }: PageProps): Promise<React.ReactElement> => {
+    const params = await paramsProps;
+    const { uri, slug } = getUriFromParams(params?.slug);
+
+    const data = await getPagesData({ uri, slug });
+
+    return (
+        <DynamicElement
+            handles={data?.typeHandle}
+            component={PAGES_INDEX_HANDLES?.[data?.type as keyof typeof PAGES_INDEX_HANDLES] as any}
+            props={data}
+        />
+    );
+};
+
+export default Page;
---
+++ components/pages/handles.ts
@@ -0,0 +1,4 @@
+export const PAGES_HANDLES = {
+    PRODUCT_DETAIL: 'sectionProducts',
+    PRODUCT_LISTING: 'sectionProductsCategories',
+} as const;
