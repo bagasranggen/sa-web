0064cae4d0a8e76ab0c7350e223b30f70f062f25
Bagas Ranggen <bagas.ranggen@gmail.com>
2026-03-03 00:02:38 +0700

    refactor: update get env libs

--- libs/utils/getEnv.ts
+++ libs/utils/getEnv.ts
@@ -1,5 +1,6 @@
 export const getEnv = () => {
     return {
         isProduction: process.env.NODE_ENV === 'production',
+        replaceHref: process.env.NEXT_PUBLIC_LINK_REPLACE?.split(','),
     };
 };
