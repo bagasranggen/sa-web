459d5bb25e38a8eef301f200709b11f879c93f4f
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2026-03-11 15:56:15 +0700

    chore: update naming

--- graphql/queries/indexes/ProductListingLoadQuery.ts
+++ graphql/queries/indexes/ProductListingLoadQuery.ts
@@ -4,7 +4,7 @@ import { FRAGMENT_PRODUCT_BASE } from '@/graphql/queries/indexes/fragments/Fragm
 import { FRAGMENT_PRODUCT_FILTERS } from '@/graphql/queries/indexes/fragments/FragmentProductFilters';
 
 export const PRODUCT_LISTING_LOAD_QUERY = gql`
-    query ProductListingQuery($categoryId: [JSON], $colorId: [JSON], $sizeId: [JSON], $limit: Int, $page: Int) {
+    query ProductListingLoadQuery($categoryId: [JSON], $colorId: [JSON], $sizeId: [JSON], $limit: Int, $page: Int) {
         Products(
             limit: $limit
             page: $page
