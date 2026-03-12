471b472c8a24d07e845606e328549cffbd39daee
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2026-03-12 14:33:12 +0700

    refactor: set env to toggle animation

--- components/common/Animation/index.tsx
+++ components/common/Animation/index.tsx
@@ -3,6 +3,7 @@
 import React, { cloneElement, RefObject, useRef } from 'react';
 
 import { AnimationProps as BaseAnimationProps } from '@/libs/@types';
+import { getEnv } from '@/libs/utils';
 
 import { gsap } from 'gsap';
 import { useGSAP } from '@gsap/react';
@@ -20,6 +21,8 @@ export type AnimationProps = {
 } & (BaseAnimationProps & Partial<Pick<HTMLElement, 'id'>>);
 
 const Animation = ({ as, type, children, trigger, id, ...props }: AnimationProps): React.ReactElement => {
+    const { enableAnimation } = getEnv();
+
     let animationRef = useRef<HTMLElement | null>(null);
     if ('ref' in children && children?.ref) animationRef = children?.ref as RefObject<HTMLElement | null>;
 
@@ -40,6 +43,7 @@ const Animation = ({ as, type, children, trigger, id, ...props }: AnimationProps
 
     useGSAP(
         () => {
+            if (!enableAnimation) return;
             if (as) return;
             if (!type) return;
             if (type && !gsap.effects[type]) {
@@ -56,7 +60,7 @@ const Animation = ({ as, type, children, trigger, id, ...props }: AnimationProps
     );
 
     // eslint-disable-next-line react-hooks/refs
-    return cloneElement(children, animationProps);
+    return cloneElement(children, enableAnimation ? animationProps : {});
 };
 
 export default Animation;
