cf407ab79d3870e7280ce00c574786499f694b47
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2026-03-12 11:30:46 +0700

    chore: cleanup codes

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,6 @@ export default async function RootLayout({ children }: Readonly<{ children: Reac
                 <body className={`${aboreto.variable} ${spaceGrotesk.variable}`}>
                     <Apollo>
                         <Main>
-                            {/*TODO: Inspect as this header cause hydration issue*/}
                             <Header items={header} />
 
                             <main>{children}</main>
