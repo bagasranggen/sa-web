48710590270493ad391fedf01645ef200b689f7a
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2026-03-14 10:01:58 +0700

    refactor: update product detail query

--- graphql/queries/indexes/ProductDetailIndexQuery.ts
+++ graphql/queries/indexes/ProductDetailIndexQuery.ts
@@ -3,6 +3,7 @@ import { gql } from '@apollo/client';
 import { FRAGMENT_PRODUCT_BASE } from '@/graphql/queries/indexes/fragments/FragmentProductBase';
 import { FRAGMENT_PRODUCT_FILTERS } from '@/graphql/queries/indexes/fragments/FragmentProductFilters';
 import { FRAGMENT_PRODUCT_DETAIL } from '@/graphql/queries/indexes/fragments/FragmentProductDetail';
+import { FRAGMENT_MEDIA } from '@/graphql/queries/common/FragmentMedia';
 
 export const PRODUCT_DETAIL_INDEX_QUERY = gql`
     query ProductDetailIndexQuery($uri: String) {
@@ -11,6 +12,10 @@ export const PRODUCT_DETAIL_INDEX_QUERY = gql`
                 ...productBase
                 ...productFilters
                 ...productDetail
+
+                media {
+                    ...productMediaDetail
+                }
             }
         }
     }
@@ -18,4 +23,9 @@ export const PRODUCT_DETAIL_INDEX_QUERY = gql`
     ${FRAGMENT_PRODUCT_BASE}
     ${FRAGMENT_PRODUCT_FILTERS}
     ${FRAGMENT_PRODUCT_DETAIL}
+    ${FRAGMENT_MEDIA({
+        on: 'MediaProduct',
+        name: 'productMediaDetail',
+        sizesHandles: ['assets1000xauto', 'assets1000x1400'],
+    })}
 `;
