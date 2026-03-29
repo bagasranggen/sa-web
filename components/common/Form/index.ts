e872068e71f010da8eaa6475ce44afb05849d42d
Bagas Ranggen <bagas.ranggen@gmail.com>
2026-03-29 18:57:31 +0700

    refactor: add form search

--- components/common/Form/index.ts
+++ components/common/Form/index.ts
@@ -1,11 +1,14 @@
 import { Component } from '@/libs/@types';
 
 import Order, { OrderProps } from '@/components/common/Form/Order';
+import Search, { SearchProps } from '@/components/common/Form/Search';
 
 export type * from '@/components/common/Form/Order';
+export type * from '@/components/common/Form/Search';
 
 type FormComposition = {
     Order: Component<OrderProps>;
+    Search: Component<SearchProps>;
 };
 
-export default Object.assign<{}, FormComposition>({}, { Order });
+export default Object.assign<{}, FormComposition>({}, { Order, Search });
