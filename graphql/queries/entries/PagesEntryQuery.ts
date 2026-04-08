579ba6165b9a741d323bbdd7abd030fe79a2b7c2
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2026-04-08 10:27:03 +0700

    refactor: set dynamic pages uri

--- graphql/queries/entries/PagesEntryQuery.ts
+++ graphql/queries/entries/PagesEntryQuery.ts
@@ -4,9 +4,11 @@ export const PAGES_ENTRY_QUERY = gql`
     query PagesEntryQuery(
         $limit: Int
         $uri: String
+        $pagesTypeInclude: Boolean! = true
         $pagesTypeHandle: Page_typeHandle_Input
+        $productsTypeInclude: Boolean! = true
         $productsTypeHandle: Product_typeHandle_Input
-        $productsCategory: JSON
+        $productsCategory: [JSON]
     ) {
         Products(
             limit: $limit
@@ -14,20 +16,18 @@ export const PAGES_ENTRY_QUERY = gql`
                 AND: [
                     { uri: { equals: $uri } }
                     { typeHandle: { equals: $productsTypeHandle } }
-                    { category: { equals: $productsCategory } }
+                    { category: { in: $productsCategory } }
                 ]
             }
-        ) {
+        ) @include(if: $productsTypeInclude) {
             docs {
                 typeHandle
                 uri
             }
         }
 
-        Pages(
-            limit: $limit
-            where: { AND: [{ uri: { equals: $uri } }, { typeHandle: { equals: $pagesTypeHandle } }] }
-        ) {
+        Pages(limit: $limit, where: { AND: [{ uri: { equals: $uri } }, { typeHandle: { equals: $pagesTypeHandle } }] })
+            @include(if: $pagesTypeInclude) {
             docs {
                 typeHandle
                 uri
--- libs/utils/getPagesUri.ts
+++ libs/utils/getPagesUri.ts
@@ -1,65 +1,93 @@
-import { Product } from '@/libs/@types';
+import { Category, Page, Product } from '@/libs/@types';
+import { getEnv } from '@/libs/utils/getEnv';
 
-import { apolloClient } from '@/libs/fetchers';
-import { PAGES_ENTRY_QUERY } from '@/graphql';
+import { apolloClient, OperationVariables } from '@/libs/fetchers';
+import { CATEGORY_ENTRY_QUERY, PAGES_ENTRY_QUERY } from '@/graphql';
 
 import { PAGES_HANDLES } from '@/components/pages/handles';
 
 export type GetPagesUriProps = {
-    typeHandles: string[];
+    typeHandles?: string[];
 };
 
 export const getPagesUri = async ({ typeHandles }: GetPagesUriProps) => {
+    const { prerenderLimitProducts, prerenderLimitPages } = getEnv();
+
     const uris = [{ slug: [''] }];
 
-    // const { data } = await apolloClient().query({
-    //     query: PAGES_ENTRY_QUERY,
-    // });
+    const getUri = async ({ variables }: { variables: OperationVariables }) => {
+        const { data } = (await apolloClient().query({
+            query: PAGES_ENTRY_QUERY,
+            variables,
+        })) as any;
 
-    // console.log({ data });
+        let docs = [];
+        if (data?.Products && data.Products.docs.length > 0) docs = data.Products.docs;
+        if (data?.Pages && data.Pages.docs.length > 0) docs = data.Pages.docs;
+
+        if (docs.length > 0) {
+            docs.forEach((itm: Product | Page) => {
+                if (itm?.uri && itm.uri.length > 0) uris.push({ slug: itm.uri.split('/') });
+            });
+        }
+    };
 
     if (typeHandles && typeHandles.length > 0) {
         for (const item of typeHandles) {
+            let limit: undefined | number = undefined;
+
             try {
                 const isSectionProducts = item === PAGES_HANDLES.PRODUCT_DETAIL;
 
-                let variables = {
-                    [isSectionProducts ? 'productsTypeHandle' : 'pagesTypeHandle']: item,
-                };
+                if (isSectionProducts) {
+                    if (prerenderLimitProducts && prerenderLimitProducts > 0) limit = prerenderLimitProducts;
 
-                // if(item !== PAGES_HANDLES.PRODUCT_DETAIL) variables = Object.assign(variables, { pagesTypeHandle: item });
-                // if(item === PAGES_HANDLES.PRODUCT_DETAIL) variables = Object.assign(variables, { productsTypeHandle: item });
+                    const categories: number[] = [];
 
-                const { data } = (await apolloClient().query({
-                    query: PAGES_ENTRY_QUERY,
-                    variables,
-                })) as any;
+                    try {
+                        const { data } = (await apolloClient().query({
+                            query: CATEGORY_ENTRY_QUERY,
+                        })) as any;
 
-                if (data?.Products && data.Products.docs.length > 0) {
-                    data.Products.docs.forEach((product: Product) => {
-                        if (product?.uri && product.uri.length > 0) uris.push({ slug: product.uri.split('/') });
-                        // uris.push({ slug: [product.slug] });
-                    });
+                        const categoriesData = data?.Categories?.docs;
+
+                        if (categoriesData && categoriesData.length > 0) {
+                            categoriesData.forEach((category: Category) => {
+                                if (category?.id) categories.push(category.id);
+                            });
+                        }
+                    } catch {}
+
+                    if (categories.length > 0) {
+                        for (const category of categories) {
+                            await getUri({
+                                variables: {
+                                    limit,
+                                    productsTypeHandle: item,
+                                    pagesTypeInclude: false,
+                                    productsCategory: category,
+                                },
+                            });
+                        }
+                    }
                 }
 
-                // console.log({ type: item, data });
-                // console.log({ product: data?.Products?.docs });
+                if (!isSectionProducts) {
+                    if (prerenderLimitPages && prerenderLimitPages > 0) limit = prerenderLimitPages;
+
+                    await getUri({
+                        variables: {
+                            limit,
+                            pagesTypeHandle: item,
+                            productsTypeInclude: false,
+                        },
+                    });
+                }
             } catch (e) {
                 console.log(e);
             }
-
-            // console.log({ item });
         }
     }
-
-    // console.log({ uris });
-
-    // return [
-    //     { slug: [''] },
-    //     { slug: ['collection'] },
-    //     { slug: ['collection', 'prom'] },
-    //     { slug: ['collection', 'wedding-guest'] },
-    // ];
 
     return uris;
 };
